59dbb378e0622cf389dd74b72df7c7a99be5dae3
Parulpathakkk <lifesick1998@gmail.com>
2024-05-25 19:48:24 +0530

    HTTP methods

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-
+const url = require("url");
 
 // const myServer2 = http.createServer((req,res)=>{
 //     console.log("hello");
@@ -8,20 +8,28 @@ const fs = require("fs");
 // })
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()} : ${req.url} New Req Received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
-        res.end("Homepage");
+        if (req.method === "GET") res.end("Homepage");
+
         break;
       case "/about":
-        res.end("about");
+        const username = myUrl.query.myname; // url search queries
+        res.end(`Hi, ${username}`);
         break;
+      case "/signup":
+        if (req.method === "GET") res.end("this is a form");
+        else if (req.method === "POST") res.end("success");
       default:
         res.end("404");
     }
   });
 });
 
-myServer2.listen(8000, () => console.log("Sever started"));
-// myServer.listen(8000, () => console.log("Sever started"));
+// myServer2.listen(8000, () => console.log("Sever started"));
+myServer.listen(8000, () => console.log("Sever started"));
